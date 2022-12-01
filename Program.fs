27ff60b4e1c7f0b527cef5222591a64952a4fc04
Varun Sethu <varun.sethu@outlook.com>
2022-12-01 17:03:20 +1100

    dayone

--- Program.fs
+++ Program.fs
@@ -2,16 +2,20 @@
 open System.IO
 open Solutions
 
+type solver = seq<string> -> string
+type problemSolution = { partOne: solver; partTwo: solver }
+
 let problemRunners = Map [
-    (1, Solutions.one)
+    (1, { partOne = Solutions.onePartOne; partTwo = Solutions.onePartTwo})
 ]
 
 [<EntryPoint>]
 let main args =
     let day = args[0] |> int
+    let part = args[1] |> int
 
     match problemRunners.TryFind day with
-    | Some solver -> System.IO.File.ReadLines($"Inputs/{day}.txt") |> solver |> printf "%s"
-    | None -> printf $"no solver for day {day} exists\n"
+    | Some solvers -> System.IO.File.ReadLines($"Inputs/{day}.txt") |> (if part = 1 then solvers.partOne else onePartTwo) |> printf "%s\n"
+    | None -> printf $"no solver for day {day} part {part} exists\n"
 
     0
--- Solutions/1.fs
+++ Solutions/1.fs
@@ -1,3 +1,13 @@
 module Solutions
 
-let one (inp: seq<string>): string = "hello"
+open System
+
+let rec getCalorieRecords = function
+    | []         -> [[]]
+    | "" :: xs   -> [] :: getCalorieRecords xs
+    | x :: xs    -> getCalorieRecords xs |> fun bs -> (List.head bs @ [int x]) :: List.tail bs
+
+let getNetCaloriesRecord: seq<string> -> int list = List.ofSeq >> getCalorieRecords >> List.map List.sum >> List.sortByDescending id
+
+let onePartOne: seq<string> -> string = getNetCaloriesRecord >> List.head >> string
+let onePartTwo: seq<string> -> string = getNetCaloriesRecord >> List.take 3 >> List.sum >> string
