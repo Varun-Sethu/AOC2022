3ed7f4c915c76fcd0a36ea6b83f680299246f3fb
Varun Sethu <varun.sethu@outlook.com>
2022-12-04 20:37:46 +1100

    thing

--- Solutions/4.fs
+++ Solutions/4.fs
@@ -8,11 +8,11 @@ let parseRangePair (pair: string) =
         |> fun [| a; b |] -> (parseRange a, parseRange b)
 
 let contains range x = range.rangeStart <= x && x <= range.rangeEnd
-let rangesOverlap (a: range, b: range) = max a.rangeStart b.rangeStart <= min a.rangeEnd b.rangeEnd
 let rangesEngulf (a: range, b: range) = 
     (contains a b.rangeStart && contains a b.rangeEnd) || 
     (contains b a.rangeStart && contains b a.rangeEnd)
 
+let rangesOverlap (a: range, b: range) = max a.rangeStart b.rangeStart <= min a.rangeEnd b.rangeEnd
 
 let partOne: string seq -> string = Seq.filter (parseRangePair >> rangesEngulf) >> Seq.length >> string
 let partTwo: string seq -> string = Seq.filter (parseRangePair >> rangesOverlap) >> Seq.length >> string
