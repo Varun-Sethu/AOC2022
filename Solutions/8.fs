aa5f1a8aaf41ddc92697a2a5fb9820948157c9e1
Varun Sethu <varun.sethu@outlook.com>
2022-12-09 01:04:00 +1100

    mek ugly

--- Solutions/8.fs
+++ Solutions/8.fs
@@ -1,14 +1,15 @@
 module Solutions.Eight
 
 let parseGrid: string seq -> int array array = Seq.map (Seq.toArray >> Array.map (fun c -> int c - int '0')) >> Seq.toArray
-let isBorderNode (numRows, numCols) (row, col) = row = 0 || col = 0 || row = numRows - 1 || col = numCols - 1
+let isOnBorder (numRows, numCols) (row, col) = row = 0 || col = 0 || row = numRows - 1 || col = numCols - 1
 
-let existsIndexGreaterThan x = Seq.tryFindIndex (fun v -> v >= x) >> Option.isSome
+let existsTreeGreaterThan x = Seq.tryFindIndex (fun v -> v >= x) >> Option.isSome
 let numVisibleTrees x seq = match Seq.tryFindIndex (fun v -> v >= x) seq with
                                 | Some i -> i + 1
                                 | None   -> Seq.length seq
 
-let visibilityDirections (grid: int array array) numRows numCols (row, col) =
+let numTreesWhere pred = List.filter pred >> List.length
+let potentialBlockingTrees (grid: int array array) numRows numCols (row, col) =
     seq {
         seq { for col' = col - 1 downto 0 do yield grid[row][col'] }; // left
         seq { for col' in col + 1 .. numCols - 1 do yield grid[row][col'] }; // right
@@ -18,29 +19,29 @@ let visibilityDirections (grid: int array array) numRows numCols (row, col) =
 
 
 let isVisibleIn (grid: int array array) (numRows, numCols) (row, col) = 
-    if isBorderNode (numRows, numCols) (row, col) then true
+    if isOnBorder (numRows, numCols) (row, col) then true
     else
-        visibilityDirections grid numRows numCols (row, col)
-            |> Seq.exists (existsIndexGreaterThan (grid[row][col]) >> not)
+        potentialBlockingTrees grid numRows numCols (row, col)
+            |> Seq.exists (fun trees -> not (existsTreeGreaterThan (grid[row][col]) trees))
 
 let scenicScoreFor (grid: int array array) (numRows, numCols) (row, col) = 
-    if isBorderNode (numRows, numCols) (row, col) then 0
+    if isOnBorder (numRows, numCols) (row, col) then 0
     else
-        visibilityDirections grid numRows numCols (row, col)
+        potentialBlockingTrees grid numRows numCols (row, col)
             |> Seq.map (numVisibleTrees (grid[row][col]))
             |> Seq.fold (*) 1
 
 
-let partOne (unparsedGrid: string seq) = 
+let partOne unparsedGrid = 
     let grid = parseGrid unparsedGrid
     let (numRows, numCols) = (grid.Length, grid[0].Length)
-    
     let isVisible = isVisibleIn grid (numRows, numCols)
-    List.allPairs [0 .. numRows - 1] [0 .. numCols - 1] |> List.filter (fun (row, col) -> isVisible (row, col)) |> List.length |> string
+    
+    List.allPairs [0 .. numRows - 1] [0 .. numCols - 1] |> numTreesWhere (fun (row, col) -> isVisible (row, col)) |> string
 
-let partTwo (unparsedGrid: string seq) =
+let partTwo unparsedGrid =
     let grid = parseGrid unparsedGrid
     let (numRows, numCols) = (grid.Length, grid[0].Length)
-
     let getScenicScore = scenicScoreFor grid (numRows, numCols)
+
     List.allPairs [0 .. numRows - 1] [0 .. numCols - 1] |> List.map getScenicScore |> List.max |> string
