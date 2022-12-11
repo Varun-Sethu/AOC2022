5847a9eb6bc2bc49a56e3ed32ab6e52598bbc95d
Varun Sethu <varun.sethu@outlook.com>
2022-12-11 13:47:18 +1100

    fix bad ting

--- Solutions/9.fs
+++ Solutions/9.fs
@@ -1,16 +1,17 @@
 module Solutions.Nine
 
 type Rope = (int * int) list
-type State = Rope * ((int * int) Set)
 type Direction = Up | Down | Left | Right
     with
-        static member emptyState (ropeSize: int): State = (List.init ropeSize (fun _ -> (0, 0)), Set.singleton (0, 0))
         member this.GetDelta ()
             = match this with
               | Up    -> (0, +1)
               | Down  -> (0, -1)
               | Left  -> (-1, 0)
               | Right -> (+1, 0)
+type State = State of Rope * ((int * int) Set)
+    with
+        static member emptyState (ropeSize: int): State = State (List.init ropeSize (fun _ -> (0, 0)), Set.singleton (0, 0))
 
 // getElementDeltas computes the deltas in wich the next element moves given the previous elements position
 let normalise x = if x = 0 then 0 else (if x <= -1 then -1 else 1)
@@ -31,12 +32,12 @@ let rec moveRopeBody prevElemPosition body =
             (newX, newY) :: (moveRopeBody (newX, newY) remElems)
 
 // we assume that the provided directions are always valid
-let moveRope (moveDir: Direction) (((headX, headY) :: rem, visited): State): State = 
+let moveRope (moveDir: Direction) (State ((headX, headY) :: rem, visited)): State = 
     let (hDeltaX, hDeltaY) = moveDir.GetDelta()
     let newHead = (headX + hDeltaX, headY + hDeltaY)
     let movedRope = newHead :: moveRopeBody newHead rem
 
-    (movedRope, visited.Add (List.last movedRope))
+    State (movedRope, visited.Add (List.last movedRope))
 
 let repeat n fn = List.init n (fun _ -> fn) |> List.reduce (>>)
 let parseCommand (x: string) = 
@@ -46,7 +47,7 @@ let parseCommand (x: string) =
         | [| "U"; amt |] -> moveRope Up |> repeat (int amt)
         | [| "D"; amt |] -> moveRope Down |> repeat (int amt)
 
-let solve (ropeSize: int) = Seq.map (parseCommand) >> Seq.fold (fun acc f -> f acc) (Direction.emptyState ropeSize) >> (fun (_, positions) -> positions.Count) >> string 
 
+let solve (ropeSize: int) = Seq.map (parseCommand) >> Seq.fold (fun acc f -> f acc) (State.emptyState ropeSize) >> (fun (State (_, positions)) -> positions.Count) >> string 
 let partOne: string seq -> string = solve 2
 let partTwo: string seq -> string = solve 10
